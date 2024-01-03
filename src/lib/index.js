63f9795d382d612e89e5f8621875ca07ac7fd44d
Gavrilencu <115806851+Gavrilencu@users.noreply.github.com>
2024-01-03 11:06:44 +0200

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,36 @@
 // place files you want to import through the `$lib` alias in this folder.
+/////////------------Auth
+import { goto } from "$app/navigation";
+export async function Autentification() {
+  let url = "/security/kerberos-login";
+  if (import.meta.env.PROD === true) {
+    url = "/security/kerberos-login";
+  } else {
+    url = "/security/developerIP-login";
+  }
+  let response = await fetch(url);
+  const data = await response.json();
+  if (response.ok) {
+    sessionStorage.setItem("username", data.DisplayName);
+    setTimeout(() => {
+      goto("/feedback/home");
+    }, 2000);
+  } else {
+    console.log("Error autentification");
+  }
+}
+
+//auth
+export async function Auth() {
+  let url = "/security/kerberos-login";
+  if (import.meta.env.PROD === true) {
+    url = "/security/kerberos-login";
+  } else {
+    url = "/security/developerIP-login";
+  }
+  let response = await fetch(url);
+  const data = await response.json();
+  if (response.ok) {
+    sessionStorage.setItem("username", data.DisplayName);
+  }
+}
